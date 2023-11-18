0423c68dac9a3769bc9fbecadea044abcc206c34
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-11-18 11:57:07 +0100

    Fix crash between Essence of the god and tropicraft
    java.lang.ClassCastException: net.essence.blocks.BlockChristmasLights cannot be cast to net.tropicraft.block.BlockCoffeePlant
    	at net.tropicraft.client.renderer.block.CoffeePlantRenderHandler.renderWorldBlock(CoffeePlantRenderHandler.java:22)
    	at cpw.mods.fml.client.registry.RenderingRegistry.renderWorldBlock(RenderingRegistry.java:118)
    	at FMLRenderAccessLibrary.renderWorldBlock(FMLRenderAccessLibrary.java:53)
    	at net.minecraft.client.renderer.RenderBlocks.func_147805_b(RenderBlocks.java:296)
    	at net.minecraft.client.renderer.WorldRenderer.func_147892_a(WorldRenderer.java:181)
    	at org.embeddedt.archaicfix.occlusion.OcclusionRenderer.rebuildChunks(OcclusionRenderer.java:255)
    	at org.embeddedt.archaicfix.occlusion.OcclusionRenderer.performCullingUpdates(OcclusionRenderer.java:312)
    	at net.minecraft.client.renderer.RenderGlobal.handler$zhi000$archaicfix$performCullingUpdates(RenderGlobal.java:2538)
    	at net.minecraft.client.renderer.RenderGlobal.func_72716_a(RenderGlobal.java)
    	at net.minecraft.client.renderer.EntityRenderer.func_78471_a(EntityRenderer.java:1187)
    	at net.minecraft.client.renderer.EntityRenderer.func_78480_b(EntityRenderer.java:1015)
    	at net.minecraft.client.Minecraft.func_71411_J(Minecraft.java:1001)
    	at net.minecraft.client.Minecraft.func_99999_d(Minecraft.java:6345)
    	at net.minecraft.client.main.Main.main(SourceFile:148)
    	at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
    	at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)
    	at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
    	at java.lang.reflect.Method.invoke(Method.java:498)
    	at net.minecraft.launchwrapper.Launch.launch(Launch.java:135)
    	at net.minecraft.launchwrapper.Launch.main(Launch.java:28)
    	at org.prismlauncher.launcher.impl.StandardLauncher.launch(StandardLauncher.java:88)
    	at org.prismlauncher.EntryPoint.listen(EntryPoint.java:126)
    	at org.prismlauncher.EntryPoint.main(EntryPoint.java:71)

--- src/main/java/net/tropicraft/client/renderer/block/CoffeePlantRenderHandler.java
+++ src/main/java/net/tropicraft/client/renderer/block/CoffeePlantRenderHandler.java
@@ -11,19 +11,24 @@ public class CoffeePlantRenderHandler implements ISimpleBlockRenderingHandler
 {
     public void renderInventoryBlock(final Block block, final int metadata, final int modelId, final RenderBlocks renderer) {
     }
-    
+
     public boolean renderWorldBlock(final IBlockAccess world, final int x, final int y, final int z, final Block block, final int modelId, final RenderBlocks renderer) {
-        renderer.overrideBlockTexture = ((BlockCoffeePlant)block).stemIcon;
-        renderer.renderCrossedSquares(block, x, y, z);
-        renderer.overrideBlockTexture = null;
-        renderer.renderStandardBlock(block, x, y, z);
-        return true;
+        if (block instanceof BlockCoffeePlant) {
+            renderer.overrideBlockTexture = ((BlockCoffeePlant) block).stemIcon;
+            renderer.renderCrossedSquares(block, x, y, z);
+            renderer.overrideBlockTexture = null;
+            renderer.renderStandardBlock(block, x, y, z);
+            return true;
+        } else {
+            return false;
+        }
     }
-    
+
+
     public boolean shouldRender3DInInventory(final int modelId) {
         return false;
     }
-    
+
     public int getRenderId() {
         return TCRenderIDs.coffeePlant;
     }
