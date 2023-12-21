03fbd25684af6a66f0f1a8bd02b30567387b24a2
QUENTIN453 <quentinlamauve@gmail.com>
2023-12-21 01:44:03 +0100

    Update WorldGenTualang.java

--- src/main/java/net/tropicraft/world/worldgen/WorldGenTualang.java
+++ src/main/java/net/tropicraft/world/worldgen/WorldGenTualang.java
@@ -10,9 +10,7 @@ import net.tropicraft.registry.*;
 public class WorldGenTualang extends TCGenBase {
 
     private static final Block WOOD_BLOCK;
-    private static final int WOOD_META = 1;
     private static final Block LEAF_BLOCK;
-    private static final int LEAF_META = 1;
     private final int baseHeight;
     private final int maxHeight;
 
@@ -48,34 +46,13 @@ public class WorldGenTualang extends TCGenBase {
 
         buildTrunk(i, j, k, height);
 
-        generateBranches(i, j, k, height, branches);
+        generateBranches(i, k, height, branches);
 
         return true;
     }
-
     private boolean checkBlocks(int i, int j, int k, int height) {
         for (int y = j; y < j + height; ++y) {
             Block block = this.worldObj.getBlock(i, y, k);
-            if (block != null && block.getMaterial().isOpaque()) {
-                return true;
-            }
-
-            block = this.worldObj.getBlock(i - 1, y, k);
-            if (block != null && block.getMaterial().isOpaque()) {
-                return true;
-            }
-
-            block = this.worldObj.getBlock(i + 1, y, k);
-            if (block != null && block.getMaterial().isOpaque()) {
-                return true;
-            }
-
-            block = this.worldObj.getBlock(i, y, k - 1);
-            if (block != null && block.getMaterial().isOpaque()) {
-                return true;
-            }
-
-            block = this.worldObj.getBlock(i, y, k + 1);
             if (block != null && block.getMaterial().isOpaque()) {
                 return true;
             }
@@ -84,24 +61,28 @@ public class WorldGenTualang extends TCGenBase {
     }
 
     private void setBaseBlocks(int i, int j, int k) {
-        this.worldObj.setBlock(i, j, k, Blocks.dirt, 0, WorldGenTualang.blockGenNotifyFlag);
-        this.worldObj.setBlock(i - 1, j, k, Blocks.dirt, 0, WorldGenTualang.blockGenNotifyFlag);
-        this.worldObj.setBlock(i + 1, j, k, Blocks.dirt, 0, WorldGenTualang.blockGenNotifyFlag);
-        this.worldObj.setBlock(i, j, k - 1, Blocks.dirt, 0, WorldGenTualang.blockGenNotifyFlag);
-        this.worldObj.setBlock(i, j, k + 1, Blocks.dirt, 0, WorldGenTualang.blockGenNotifyFlag);
-    }
-
-    private void buildTrunk(int i, int j, int k, int height) {
-        for (int y2 = j; y2 < height; ++y2) {
-            for (int x = i - 1; x <= i + 1; ++x) {
-                for (int z = k - 1; z <= k + 1; ++z) {
-                    this.worldObj.setBlock(x, y2, z, WorldGenTualang.WOOD_BLOCK, 1, WorldGenTualang.blockGenNotifyFlag);
+        for (int xOffset = -1; xOffset <= 1; xOffset++) {
+            for (int zOffset = -1; zOffset <= 1; zOffset++) {
+                if (xOffset == 0 && zOffset == 0) {
+                    this.worldObj.setBlock(i + xOffset, j, k + zOffset, Blocks.dirt, 0, WorldGenTualang.blockGenNotifyFlag);
+                } else {
+                    this.worldObj.setBlock(i + xOffset, j, k + zOffset, Blocks.dirt, 0, 2);
                 }
             }
         }
     }
 
-    private void generateBranches(int i, int j, int k, int height, int branches) {
+    private void buildTrunk(int i, int j, int k, int height) {
+        for (int y = j; y < height; ++y) {
+            for (int xOffset = -1; xOffset <= 1; xOffset++) {
+                for (int zOffset = -1; zOffset <= 1; zOffset++) {
+                    this.worldObj.setBlock(i + xOffset, y, k + zOffset, WorldGenTualang.WOOD_BLOCK, 1, 2);
+                }
+            }
+        }
+    }
+
+    private void generateBranches(int i, int k, int height, int branches) {
         for (int x = 0; x < branches; ++x) {
             final int branchHeight = this.rand.nextInt(4) + 2 + height;
             final int bx = this.rand.nextInt(15) - 8 + i;
@@ -109,13 +90,16 @@ public class WorldGenTualang extends TCGenBase {
 
             int midX = i + this.sign((bx - i) / 2);
             int midZ = k + this.sign((bz - k) / 2);
-            this.placeBlockLine(new int[]{midX, height, midZ}, new int[]{bx, branchHeight, bz},
-                WorldGenTualang.WOOD_BLOCK, 1);
+
+            for (int y = height; y <= branchHeight; y++) {
+                this.worldObj.setBlock(midX, y, midZ, WorldGenTualang.WOOD_BLOCK, 1, WorldGenTualang.blockGenNotifyFlag);
+            }
 
             this.genCircle(bx, branchHeight, bz, 2.0, 1.0, WorldGenTualang.LEAF_BLOCK, 1, false);
             this.genCircle(bx, branchHeight + 1, bz, 3.0, 2.0, WorldGenTualang.LEAF_BLOCK, 1, false);
         }
     }
+
 
     private int sign(final int i) {
         return (i == 0) ? 0 : ((i <= 0) ? -1 : 1);
